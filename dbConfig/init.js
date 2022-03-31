b7b460e3ae892bd8c664f94309481ef8e57cd25c
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-03-31 23:33:18 +0100

    modify config init file to allow deploys

--- dbConfig/init.js
+++ dbConfig/init.js
@@ -1,5 +1,15 @@
 const { Pool } = require("pg");
 
+let config;
+
+if (process.env.DATABASE_URL) {
+	config = {
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false
+		}
+	}
+}
 const pool = new Pool();
 
 module.exports = pool;
